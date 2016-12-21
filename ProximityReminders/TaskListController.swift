9c634f919a5df236b5329c1bebb9cc62fad01924
bonuelc <bonuelc@gmail.com>
2016-12-21 13:53:46 -0700

    Add class TaskListController: UITableViewController
    # Conflicts:
    #	ProximityReminders.xcodeproj/project.pbxproj

---
+++ ProximityReminders/TaskListController.swift
@@ -0,0 +1,13 @@
+//
+//  TaskListController.swift
+//  ProximityReminders
+//
+//  Created by Christopher Bonuel on 12/21/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TaskListController: UITableViewController {
+}
