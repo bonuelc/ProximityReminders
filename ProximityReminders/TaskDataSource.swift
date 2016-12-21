4417dc683ed450d727e6002044307a7133221d54
bonuelc <bonuelc@gmail.com>
2016-12-21 14:56:56 -0700

    Add class TaskDataSource: UITableViewDataSource
    TODO: Implement required protocol methods

---
+++ ProximityReminders/TaskDataSource.swift
@@ -0,0 +1,15 @@
+//
+//  TaskDataSource.swift
+//  ProximityReminders
+//
+//  Created by Christopher Bonuel on 12/21/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TaskDataSource: NSObject, UITableViewDataSource {
+    
+    
+}
