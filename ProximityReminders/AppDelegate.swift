85e2d5c552ed4cdb932652536a291eb5792d1a40
bonuelc <bonuelc@gmail.com>
2016-12-21 13:45:14 -0700

    Set up splitVC as root view controller

--- ProximityReminders/AppDelegate.swift
+++ ProximityReminders/AppDelegate.swift
@@ -16,7 +16,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        guard let window = window else { return false }
+        
+        let masterViewController = UIViewController()
+        let masterNavigationControler = UINavigationController(rootViewController: masterViewController)
+        
+        let splitVC = UISplitViewController()
+        splitVC.viewControllers = [masterNavigationControler]
+        
+        window.rootViewController = splitVC
+        window.makeKeyAndVisible()
+        
         return true
     }
 
