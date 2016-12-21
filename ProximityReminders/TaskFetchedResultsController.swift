0d5924a2d5e0a1af4a5ab8b64aefdff8b99d9e1a
bonuelc <bonuelc@gmail.com>
2016-12-21 14:48:42 -0700

    Add class TaskFetchedResultsController

---
+++ ProximityReminders/TaskFetchedResultsController.swift
@@ -0,0 +1,13 @@
+//
+//  TaskFetchedResultsController.swift
+//  ProximityReminders
+//
+//  Created by Christopher Bonuel on 12/21/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TaskFetchedResultsController: NSFetchedResultsController {
+}
