712b9b7de481f8370675bb5df0aeb247fd6f4fc3
bonuelc <bonuelc@gmail.com>
2016-12-21 18:43:34 -0700

    Add class LocationManager

---
+++ ProximityReminders/LocationManager.swift
@@ -0,0 +1,14 @@
+//
+//  LocationManager.swift
+//  ProximityReminders
+//
+//  Created by Christopher Bonuel on 12/21/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class LocationManager: CLLocationManager {
+
+}
