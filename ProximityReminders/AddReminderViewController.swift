56172a2381f80a9121cd7ecafc7658bc4258a97a
bonuelc <bonuelc@gmail.com>
2017-03-30 18:51:14 -0600

    Add AddReminderVC
    This VC will simply have an 'Add' button to replace the void on launch

---
+++ ProximityReminders/AddReminderViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AddReminderViewController.swift
+//  ProximityReminders
+//
+//  Created by Christopher Bonuel on 3/30/17.
+//  Copyright © 2017 Christopher Bonuel. All rights reserved.
+//
+
+import UIKit
+
+class AddReminderViewController: UIViewController {
+}
